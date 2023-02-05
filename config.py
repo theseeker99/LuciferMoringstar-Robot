88798e16c23f15dd828d3330e217ecf6a42dc58b
The Seeker <110098713+theseeker99@users.noreply.github.com>
2023-02-05 21:26:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,10 +23,10 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 PICS = os.environ.get("PICS", "https://telegra.ph/file/17a668f74d66f7f783047.jpg")
 # Database
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "vpsteaster1")
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://vpsteaster1:LHz6YGCGQt3gBPwZ@cluster0.nqm5e2l.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongo2l.mongodb.net/?retryWrites=true&w=majority")
 # Chats & Users
 ADMINS = os.environ.get("ADMINS", "1418002763")
-SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "the_seeker_s_cave")
+SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "ter_s_cave")
 AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-1001707091598")
 CHANNELS = [int(ch) if search.search(ch) else ch for ch in os.environ.get("CHANNELS", "-1001656311343").split()]
 LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001690180221")
